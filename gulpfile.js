d74467d56caa12c7df59a333d8d23c38b3f46440
Kanon Kakuno <yadex205@outlook.jp>
2016-12-19 23:17:13 +0900

    :+1: Define header design

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,17 @@ const JS_SRC = [
     'bower_components/slick-carousel/slick/slick.js',
     'src/js/**/*.js'
 ]
+const IMG_SRC = 'src/img/**/*'
 
 gulp.task('build', (next) => {
-    runSequence('clean', ['html', 'css', 'js'], next)
+    runSequence('clean', ['html', 'css', 'js', 'img'], next)
 })
 
 gulp.task('live', ['build'], () => {
     gulp.watch(HTML_SRC, ['html'])
     gulp.watch(CSS_SRC, ['css'])
     gulp.watch(JS_SRC, ['js'])
+    gulp.watch(IMG_SRC, ['img'])
     browserSync.init({
         server: { baseDir: './htdocs' }
     })
@@ -53,5 +55,11 @@ gulp.task('js', () => {
         .pipe(plug.uglify())
         .pipe(plug.concat('index.js'))
         .pipe(gulp.dest('./htdocs/js'))
+        .pipe(browserSync.stream())
+})
+
+gulp.task('img', () => {
+    return gulp.src(IMG_SRC)
+        .pipe(gulp.dest('htdocs/img'))
         .pipe(browserSync.stream())
 })
